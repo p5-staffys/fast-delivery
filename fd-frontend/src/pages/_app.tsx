3bf80fb2d0bf39bf1c9c8ff447ac9f8ef2cad363
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2023-02-15 13:04:27 -0300

    concurrent npm i

--- fd-frontend/src/pages/_app.tsx
+++ fd-frontend/src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import 'alias/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "alias/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
