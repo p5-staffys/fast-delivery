0ee5ab023ee6615c70ec1e64eb4c513fe6086637
pburgos09 <burgospablom@gmail.com>
2023-02-24 17:08:31 -0300

    feat: finish linter config front

--- fd-frontend/jest.config.ts
+++ fd-frontend/jest.config.ts
@@ -6,7 +6,7 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
 };
 
 export default createJestConfig(customJestConfig);
