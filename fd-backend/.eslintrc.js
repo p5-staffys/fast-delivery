b68ec8581cb796b520ce6bad632f6f4191e1534e
pburgos09 <burgospablom@gmail.com>
2023-02-24 16:12:01 -0300

    test: testing linter back

--- fd-backend/.eslintrc.js
+++ fd-backend/.eslintrc.js
@@ -22,9 +22,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'no-console': [
-      'warn',
-      { allow: ['clear', 'info', 'error', 'dir', 'trace', 'log'] },
-    ],
+    'no-console': 'error',
   },
 };
