dcd480d55a56ea4cca5bf71c3f1d7077c2b44e45
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2023-04-13 07:14:03 -0300

    feat: firebase-admin

--- fd-backend/src/modules/auth/admin-auth.service.ts
+++ fd-backend/src/modules/auth/admin-auth.service.ts
@@ -1,17 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
-import { initializeApp, ServiceAccount } from 'firebase-admin/app';
+import { initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
-import * as firebaseAdmin from 'firebase-admin';
-import firebaseAccountCredentials from '../../../fast-delivery-uma-firebase-adminsdk-y1cvp-422b7b2779.json';
-
-const serviceAccount = firebaseAccountCredentials as ServiceAccount;
-
-const app = initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount),
-  projectId: 'fast-delivery-uma',
-});
+const app = initializeApp();
 
 export const auth = getAuth(app);
 
