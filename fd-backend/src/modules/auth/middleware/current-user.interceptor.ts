dbb21151c14b1a31997105071156352297ab5690
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2023-04-23 18:12:10 -0300

    feat: auth bia header

--- fd-backend/src/modules/auth/middleware/current-user.interceptor.ts
+++ fd-backend/src/modules/auth/middleware/current-user.interceptor.ts
@@ -35,30 +35,6 @@ export class CurrentUserInterceptor implements NestInterceptor {
   }
 }
 
-/* @Injectable()
-export class CurrentUserInterceptor implements NestInterceptor {
-  constructor(
-    private adminAuthService: AdminAuthService,
-    private userService: UserService,
-  ) {}
-  async intercept(context: ExecutionContext, handler: CallHandler) {
-    const request = context.switchToHttp().getRequest();
-    const idToken = request.cookies['idToken'];
-    const _id = (await this.adminAuthService.authenticate(idToken)).uid;
-    if (_id) {
-      try {
-        const user = await this.userService.findById(_id);
-        request.currentUser = user;
-      } catch {
-        throw new NotFoundException(
-          'Por favor ingresa tu email y password nuevamente',
-        );
-      }
-    }
-    return handler.handle();
-  }
-} */
-
 export interface CurrentUserRequest extends Request {
-  currentUser: IUser;
+  currentUser?: IUser;
 }
