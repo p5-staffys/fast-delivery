0c48ade69175d77589a183392d1bb09e9cb023b2
pburgos09 <burgospablom@gmail.com>
2023-04-28 13:18:14 -0300

    feat: added create package

--- fd-frontend/src/app/services/packets.service.ts
+++ fd-frontend/src/app/services/packets.service.ts
@@ -1,11 +1,45 @@
-import { Package } from "@/context/store";
+import { Package, UserRef } from "@/context/store";
 import axios, { AxiosResponse } from "axios";
 
 export const getPacketById = async (_id: string): Promise<Package> => {
   try {
     const idToken = localStorage.getItem("idToken");
-    const response: AxiosResponse = await axios.get(
-      `https://fd-backend-no-cookie-buhubxjtrq-uw.a.run.app/package/${_id}`,
+    const response: AxiosResponse = await axios.get(`https://backend-buhubxjtrq-ue.a.run.app/package/${_id}`, {
+      withCredentials: true,
+      headers: { Authorization: idToken },
+    });
+    const packet: Package = response.data;
+    return packet;
+  } catch (error: unknown) {
+    throw error;
+  }
+};
+
+export const getAllPackages = async (): Promise<Package[]> => {
+  try {
+    const idToken = localStorage.getItem("idToken");
+    const response: AxiosResponse = await axios.get(`https://backend-buhubxjtrq-ue.a.run.app/package/`, {
+      withCredentials: true,
+      headers: { Authorization: idToken },
+    });
+    const packets: Package[] = response.data;
+    return packets;
+  } catch (error: unknown) {
+    throw error;
+  }
+};
+
+export const createPackage = async (
+  weight: number,
+  client: UserRef,
+  deliveryDate: string,
+  quantity: number,
+): Promise<Package> => {
+  try {
+    const idToken = localStorage.getItem("idToken");
+    const response: AxiosResponse = await axios.post(
+      `https://backend-buhubxjtrq-ue.a.run.app/package/`,
+      { weight, client: client.client, deliveryDate, quantity },
       { withCredentials: true, headers: { Authorization: idToken } },
     );
     const packet: Package = response.data;
