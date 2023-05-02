442d0fef7197df8eba32fd508512a6ea6ddf6894
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2023-05-02 09:51:16 -0300

    fix: deploy

--- fd-frontend/next.config.js
+++ fd-frontend/next.config.js
@@ -4,4 +4,9 @@ module.exports = {
   experimental: {
     appDir: true,
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
+  },
 };
