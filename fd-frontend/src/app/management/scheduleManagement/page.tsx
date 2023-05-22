839e994be05730f8f7bceb955ed11fa7483b99a9
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2023-05-22 18:23:41 -0300

    fix: vista principal del admin

--- fd-frontend/src/app/management/scheduleManagement/page.tsx
+++ fd-frontend/src/app/management/scheduleManagement/page.tsx
@@ -1,30 +1,20 @@
 "use client";
 import { Box, Container } from "@mui/system";
 import React, { useEffect, useState } from "react";
-import { Accordion, AccordionDetails, AccordionSummary, Button, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Button, Grid, Typography } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Link from "next/link";
 import DayList from "./components/carousel";
 import Progress from "./components/progress";
 import AdminGuard from "../../../utils/guards/adminGuard";
-import { getCurrentAdmin, getLogs } from "../../services/admin.service";
+import { getLogs } from "../../services/admin.service";
 import { Logs } from "@/utils/interfaces/user.interfaces";
-import { Admin } from "@/utils/interfaces/admin.interfaces";
 
 const Agenda = (): JSX.Element => {
   const today = new Date();
   const formattedDate = today.toLocaleDateString("es-ES", { dateStyle: "short" });
-  const [admin, setAdmin] = useState<Admin>();
   const [date, setDate] = useState<string>("");
   const [logs, setLogs] = useState<Logs>();
-
-  useEffect(() => {
-    const getAdmin = async (): Promise<void> => {
-      const newAdmin = await getCurrentAdmin();
-      setAdmin(newAdmin);
-    };
-    getAdmin();
-  }, []);
 
   useEffect(() => {
     const getAllLogsAsync = async (): Promise<void> => {
@@ -49,87 +39,96 @@ const Agenda = (): JSX.Element => {
 
   return (
     <AdminGuard>
-      <Container>
-        <Box sx={{ mt: 0, display: "flex", flexDirection: "inherit", alignItems: "center" }}>
-          <img
-            src={
-              admin?.avatarURL
-                ? admin.avatarURL
-                : "https://villagesonmacarthur.com/wp-content/uploads/2020/12/Blank-Avatar.png"
-            }
-            alt="logo"
-            width={53}
-            style={{ borderRadius: "50%" }}
-          />
-          <Box sx={{ paddingX: 2, mb: 3, flexGrow: 1, marginTop: "10px" }}>
-            <Typography sx={{ fontFamily: "Roboto", fontSize: "18px", fontWeight: 700 }}>Gestionar pedidos</Typography>
-          </Box>
+      <Container fixed maxWidth="md">
+        <Box sx={{ mb: 3, mt: 10 }}>
+          <Typography variant="h5">Gestionar Pedidos</Typography>
         </Box>
+
         <DayList handleDate={handleDate} />
+
+        <Typography sx={{ fontWeight: 700, fontSize: "16px" }}>{formattedDate} - Detalles</Typography>
         <Accordion sx={{ mt: 2 }} defaultExpanded={true}>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography sx={{ fontWeight: 700, fontSize: "16px" }}>{formattedDate} - Detalles</Typography>
+            <Grid container spacing={2} direction="row" justifyContent="space-between" alignItems="center">
+              <Grid item>
+                <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                  Repartidores
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1">
+                  {`${logs?.users?.activeUsers.length ?? 0}/${logs?.users?.totalUsersCount ?? 0} activos`}
+                </Typography>
+              </Grid>
+            </Grid>
           </AccordionSummary>
-          <Container>
-            <Box sx={{ width: "90vw", m: "auto" }}>
-              <AccordionDetails>
-                <Box display="flex" justifyContent="space-between">
-                  <Box>
-                    <Progress value={progressUsers} />
-                  </Box>
-                  <Box sx={{ ml: 3 }}>
-                    <Typography sx={{ fontWeight: 700, fontSize: "15px" }}> Repartidores</Typography>
-                    <Typography sx={{ fontWeight: 200, fontSize: "13px" }}>
-                      {" "}
-                      {`${logs?.users?.activeUsers.length ?? 0}/${logs?.users?.totalUsersCount ?? 0} activos`}
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <Box sx={{ mr: 3 }}>
-                      {logs?.users?.activeUsers.map((user) => (
-                        <img
-                          key={user._id}
-                          style={{ position: "absolute", right: 59, width: "60px", borderRadius: "50%" }}
-                          alt="avatar"
-                          src={
-                            user.avatarURL
-                              ? user.avatarURL
-                              : "https://villagesonmacarthur.com/wp-content/uploads/2020/12/Blank-Avatar.png"
-                          }
-                        />
-                      ))}
-                    </Box>
+
+          <Box sx={{ width: "90vw", m: "auto" }}>
+            <AccordionDetails>
+              <Box display="flex" justifyContent="space-between">
+                <Box>
+                  <Progress value={progressUsers} />
+                </Box>
+                <Box>
+                  <Box sx={{ mr: 3 }}>
+                    {logs?.users?.activeUsers.map((user) => (
+                      <img
+                        key={user._id}
+                        style={{ position: "absolute", right: 59, width: "60px", borderRadius: "50%" }}
+                        alt="avatar"
+                        src={
+                          user.avatarURL
+                            ? user.avatarURL
+                            : "https://villagesonmacarthur.com/wp-content/uploads/2020/12/Blank-Avatar.png"
+                        }
+                      />
+                    ))}
                   </Box>
                 </Box>
-              </AccordionDetails>
-            </Box>
-            <Link href="/management/scheduleManagement/manageDeliveryMen">
-              <Button sx={{ my: 2 }} variant="contained" fullWidth size="small">
+              </Box>
+            </AccordionDetails>
+          </Box>
+          <Link href="/management/scheduleManagement/manageDeliveryMen">
+            <Box sx={{ my: 2, mx: 2 }}>
+              <Button variant="contained" fullWidth size="small">
                 Ver repartidores
               </Button>
-            </Link>
-            <Box sx={{ width: "90vw", m: "auto" }}>
-              <AccordionDetails>
-                <Box display="flex" justifyContent="space-between">
-                  <Box>
-                    <Progress value={progressPackages} />
-                  </Box>
-                  <Box sx={{ mr: 12 }}>
-                    <Typography sx={{ fontWeight: 700, fontSize: "15px" }}> Paquetes</Typography>
-                    <Typography sx={{ fontWeight: 400, fontSize: "13px" }}>{`${logs?.packages?.activePackages ?? 0}/${
-                      logs?.packages?.totalPackages ?? 0
-                    } repartidos`}</Typography>
-                  </Box>
-                  <Box></Box>
-                </Box>
-              </AccordionDetails>
             </Box>
-            <Link href={`/management/scheduleManagement/managePackages/${date}`}>
-              <Button sx={{ my: 2 }} variant="contained" fullWidth size="small">
+          </Link>
+        </Accordion>
+
+        <Accordion sx={{ mt: 2 }} defaultExpanded={true}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Grid container spacing={2} direction="row" justifyContent="space-between" alignItems="center">
+              <Grid item>
+                <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+                  Paquetes
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1">
+                  {`${logs?.packages?.activePackages ?? 0}/${logs?.packages?.totalPackages ?? 0} repartidos`}
+                </Typography>
+              </Grid>
+            </Grid>
+          </AccordionSummary>
+
+          <Box sx={{ width: "90vw" }}>
+            <AccordionDetails>
+              <Box display="flex" justifyContent="space-between">
+                <Box>
+                  <Progress value={progressPackages} />
+                </Box>
+              </Box>
+            </AccordionDetails>
+          </Box>
+          <Link href={`/management/scheduleManagement/managePackages/${date}`}>
+            <Box sx={{ my: 2, mx: 2 }}>
+              <Button variant="contained" fullWidth size="small">
                 Ver paquetes
               </Button>
-            </Link>
-          </Container>
+            </Box>
+          </Link>
         </Accordion>
       </Container>
     </AdminGuard>
