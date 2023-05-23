d573d83daebd8dca1f8b3f404e223523db1e43d1
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2023-05-23 11:44:26 -0300

    fix: arreglo del form popup

--- fd-frontend/src/app/deliveryMan/workingDay/pending/components/radioGroup.tsx
+++ fd-frontend/src/app/deliveryMan/workingDay/pending/components/radioGroup.tsx
@@ -2,8 +2,7 @@ import * as React from "react";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import FormControl from "@mui/material/FormControl";
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 interface Props {
   ask: object;
@@ -12,28 +11,32 @@ interface Props {
 
 export default function ErrorRadios({ ask, handleClick }: Props): React.ReactElement {
   return (
-    <FormControl sx={{ alignItems: "center" }}>
-      <Typography mt={4} fontWeight={700} fontSize="15px" lineHeight={"20px"}>
-        {Object.values(ask)}
-      </Typography>
-      <RadioGroup row sx={{ mt: 2 }}>
-        <FormControlLabel
-          value="true"
-          control={<Radio />}
-          name={Object.keys(ask)[0]}
-          label="Si"
-          labelPlacement="bottom"
-          onChange={handleClick}
-        />
-        <FormControlLabel
-          value="false"
-          control={<Radio />}
-          name={Object.keys(ask)[0]}
-          label="No"
-          labelPlacement="bottom"
-          onChange={handleClick}
-        />
-      </RadioGroup>
-    </FormControl>
+    <Grid container flexDirection="column" alignItems="center">
+      <Grid item>
+        <Typography mt={3} variant="h6" textAlign="center">
+          {Object.values(ask)}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <RadioGroup row>
+          <FormControlLabel
+            value="true"
+            control={<Radio />}
+            name={Object.keys(ask)[0]}
+            label="Si"
+            labelPlacement="bottom"
+            onChange={handleClick}
+          />
+          <FormControlLabel
+            value="false"
+            control={<Radio />}
+            name={Object.keys(ask)[0]}
+            label="No"
+            labelPlacement="bottom"
+            onChange={handleClick}
+          />
+        </RadioGroup>
+      </Grid>
+    </Grid>
   );
 }
